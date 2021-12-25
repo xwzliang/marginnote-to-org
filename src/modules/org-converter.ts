bdcfec8ec64b9b684f6cf8ae668479921d7e14c3
broliang <18706703437@163.com>
2021-12-25 09:57:36 +0800

    Use org-converter to convert notes to org format.

---
+++ src/modules/org-converter.ts
@@ -0,0 +1,52 @@
+import {
+  Book,
+  Data,
+  Note,
+  ReturnBody,
+  ReturnBody_Note,
+  ReturnBody_Sel,
+  ReturnBody_Toc,
+  Selection,
+  Toc,
+} from "../return";
+
+export const convert_to_org = (body: ReturnBody_Toc): string => {
+  const { data: toc } = body;
+  // try {
+  const iterate = (toc: Toc, depth = 0): string => {
+    let {
+      noteId,
+      noteTitle,
+      childNotes,
+      excerptText,
+      // notesText: AllText,
+      // docMd5,
+      startPage,
+      endPage,
+    } = toc;
+    excerptText = excerptText === undefined ? "" : excerptText;
+    let rendered;
+    if (startPage === undefined || noteTitle === undefined) {
+      rendered = ""; // Skip root note
+    } else {
+      rendered =
+        "*".repeat(depth) +
+        ` ${noteTitle}
+:PROPERTIES:
+:ID:       ${noteId}
+:MARGINNOTE_LINK: [[marginnote3app://note/${noteId}][link]]
+:NOTER_PAGE: ${startPage}
+:NOTER_PAGE_END: ${endPage}
+:END:
+${excerptText}
+`;
+    }
+    const lines = Array.isArray(childNotes)
+      ? childNotes.map((t) => iterate(t, depth + 1))
+      : [];
+    lines.unshift(rendered);
+    return lines.join("\n");
+  };
+  return iterate(toc);
+  // } catch (error) {}
+};
--- src/modules/sender.ts
+++ src/modules/sender.ts
@@ -5,10 +5,14 @@ import { getBody_Note, getBody_Sel, getBody_Toc } from "./parser";
 import { copy, isMac, showHUD } from "./tools";
 import getText from "./translate";
 import { ObjToJson, ObjToUrl } from "./url-obj";
+import { convert_to_org } from "./org-converter";
 
 export const SendToc = (src: MbBookNote): void => {
   try {
-    send(getBody_Toc(src));
+    // send(getBody_Toc(src));
+    let str = convert_to_org(getBody_Toc(src));
+    // Copy to clipboard
+    copy(str);
     showHUD(getText("hint_toc_success") + src.noteTitle);
   } catch (error) {
     showHUD(error);
